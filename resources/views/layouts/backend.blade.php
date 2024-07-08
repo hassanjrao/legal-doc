230b4f5900904cd8b9c0ccee45921ef1671a5444
Hassan Javaid <hassanjrao@gmail.com>
2024-07-09 01:33:53 +0400

    change colors

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -28,7 +28,7 @@
     <link id="style" href="{{ asset('assets/libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Style Css -->
-    <link href="{{ asset('assets/css/styles.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
 
     <!-- Icons Css -->
     <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet">
